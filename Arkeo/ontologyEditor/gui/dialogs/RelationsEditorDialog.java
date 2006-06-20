30a11e682c228ecb552d9fe7981ee370a82e095e
m1isi20 <m1isi20>
2006-06-20 09:41:47 +0000

    *** empty log message ***

--- Arkeo/ontologyEditor/gui/dialogs/RelationsEditorDialog.java
+++ Arkeo/ontologyEditor/gui/dialogs/RelationsEditorDialog.java
@@ -7,320 +7,184 @@ package ontologyEditor.gui.dialogs;
 
 import info.clearthought.layout.TableLayout;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JSeparator;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
 import ontologyEditor.ApplicationManager;
-import ontologyEditor.Constants;
 import ontologyEditor.DisplayManager;
-import arkeotek.io.EncryptedProperties;
-import arkeotek.io.db.Service;
+import ontologyEditor.gui.tables.RelationsEditorTableModel;
+import arkeotek.ontology.Relation;
 
 /**
  * @author Bernadou Pierre
  * @author Czerny Jean
  *
  */
-public class RelationsEditorDialog extends JDialog  implements ActionListener, PropertyChangeListener
+public class RelationsEditorDialog extends JDialog  implements ActionListener
 {
 	private JFrame frame;
-	   
-	private JLabel lbl_ontology_name;
-	private JLabel lbl_storage_type;
-	private JLabel lbl_storage_repository;
-	private JLabel lbl_user_login;
-	private JLabel lbl_user_password;
-	private JLabel lbl_user_confirm;
 	
-	private JTextField txt_ontology_name;
-	private JTextField txt_storage_repository;
-	private JComboBox cmb_storage_type;
-	private JTextField txt_user_login;
-	private JPasswordField txt_user_password;
-	private JPasswordField txt_user_confirm;
-   
-	private static final String VALIDATE_CAPTION = "Valider";
-	private static final String CANCEL_CAPTION = "Annuler";
-
-	private Object value;
-
-	private boolean valid = false;
+	private JButton okButton = new JButton();
+	private JButton cancelButton = new JButton();
+	private JTextField textField = new JTextField();
+	private JButton addButton = new JButton();
+	private JButton delButton = new JButton();
 	
-	private JButton validate_button = new JButton(VALIDATE_CAPTION);
-	private JButton cancel_button = new JButton(CANCEL_CAPTION);
-
-	/**
-	 * 
-	 */
+	private RelationsEditorTableModel model = new RelationsEditorTableModel();
+	private JTable liste = new JTable(model);
+	
 	public RelationsEditorDialog()
 	{
 		this(DisplayManager.mainFrame);
 	}
 
- 	/**
-	 * @param frame The <code>JFrame</code> holding this <code>JPanel</code>. 
-	 */
 	private RelationsEditorDialog(JFrame frame)
 	{
         super(frame, ApplicationManager.getApplicationManager().getTraduction("relationseditor"), true);
         this.frame = frame;
-		double border = 10;
-		double separator = 5 + 2 * border;
-		double line = 30;
-		double lbl_width = 140;
-		double size[][] = { 
-				{ border, lbl_width, border, TableLayout.FILL, border }, // Columns
-				{ border, line, border, separator, line, border, line, border, separator, line, border, line, border, line, border, separator, line, border }  // Rows
-			};
+        
+        
+        
+        /*
+         * Window Disposition
+         */
+		double espacement = 20;
+		double longSpace = 90;
+		double buttonHeight = 25;
+		double buttonWidth = 105;
+		double size[][] = {  
+							{ espacement, TableLayout.FILL, buttonWidth, longSpace, espacement, buttonWidth, espacement }, // Columns
+							{ espacement, buttonHeight, buttonHeight, buttonHeight, TableLayout.FILL, espacement, buttonHeight, espacement }  // Rows
+						  };
 		this.setLayout(new TableLayout(size));
-		//this.setLocationRelativeTo(null);
-		
-        // Create the components. 
-		this.lbl_ontology_name = new JLabel("Nom de l'ontologie : "); 
-        this.add(this.lbl_ontology_name, "1, 1, 1, 1");
-		
-		this.txt_ontology_name = new JTextField("");
-		this.add(this.txt_ontology_name, "3, 1 ,3 , 1");
-		
-		this.add(new JSeparator(), "1, 3, 3, 2");
-		
-		this.lbl_storage_type = new JLabel("Type de stockage : ");
-		this.add(this.lbl_storage_type, "1, 4, 1, 3");
+		/*
+		 * 
+		 */
 
-		this.cmb_storage_type = new JComboBox();
-		this.cmb_storage_type.addItem(new StorageCell("(Choisissez un type de stockage pour vos donn\u00e9es)", ""));
-		this.cmb_storage_type.addItem(new StorageCell("Base de donn\u00e9es MySQL", Service.class.getCanonicalName()));
-		this.add(this.cmb_storage_type, "3, 4, 3, 3");
 		
-		this.lbl_storage_repository = new JLabel("Emplacement : ");
-		this.add(this.lbl_storage_repository, "1, 6, 1, 5");
+		JScrollPane borderedPane = new JScrollPane();
+		borderedPane.setBorder(BorderFactory.createTitledBorder("Relations"));
 		
-		this.txt_storage_repository = new JTextField("");
-		this.add(this.txt_storage_repository, "3, 6, 3, 5");
+		this.liste = new JTable(model);
+		borderedPane.setViewportView(liste);
+		//this.liste.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		//panel.add(this.liste);
+        this.add(borderedPane, "1, 1, 3, 4");
 		
-		this.add(new JSeparator(), "1, 8, 3, 6");
-		
-		this.lbl_user_login = new JLabel("Nom d'utilisateur : ");
-		this.add(this.lbl_user_login, "1, 9, 1, 7");
-
-		this.txt_user_login = new JTextField("");
-		this.add(this.txt_user_login, "3, 9, 3, 7");
-		
-		this.lbl_user_password = new JLabel("Mot de passe : ");
-		this.add(this.lbl_user_password, "1, 11, 1, 9");
-
-		this.txt_user_password = new JPasswordField("");
-		this.add(this.txt_user_password, "3, 11, 3, 9");
-		
-		this.lbl_user_confirm = new JLabel("Confirmez : ");
-		this.add(this.lbl_user_confirm, "1, 13, 1, 11");
-
-		this.txt_user_confirm = new JPasswordField("");
-		this.add(this.txt_user_confirm, "3, 13, 3, 11");
-		
-		this.add(new JSeparator(), "1, 15, 3, 12");
-		
-		this.validate_button.addMouseListener(new MouseAdapter()
+		okButton.setText(ApplicationManager.getApplicationManager().getTraduction("okbutton"));
+        this.add(this.okButton, "5, 6, 5, 6");
+		this.okButton.addMouseListener(new MouseAdapter()
 		{
-			/**
-			 * @see java.awt.event.MouseAdapter#mouseClicked(java.awt.event.MouseEvent)
-			 */
-			@Override
 			public void mouseClicked(MouseEvent e)
 			{
 				RelationsEditorDialog.this.validateInput();
 			}
 		});
-		this.add(this.validate_button, "3, 16, 3, 13");
-		this.cancel_button.addMouseListener(new MouseAdapter()
-				{
-					/**
-					 * @see java.awt.event.MouseAdapter#mouseClicked(java.awt.event.MouseEvent)
-					 */
-					@Override
-					public void mouseClicked(MouseEvent e)
-					{
-						RelationsEditorDialog.this.cancelInput();
-					}
-				});
-		this.add(this.cancel_button, "1, 16, 1, 13");
+
+		cancelButton.setText(ApplicationManager.getApplicationManager().getTraduction("cancelbutton"));
+		this.add(this.cancelButton, "1, 6, 2, 6");
+		this.cancelButton.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e)
+			{
+				RelationsEditorDialog.this.cancelInput();
+			}
+		});
+
+		textField.setText("");
+		this.add(textField,"5, 1, 5, 1");
 		
-		/*
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createTitledBorder("Panneau de navigation:"));
-		this.add(panel);
-		panel.add(lbl_ontology_name);
-		panel.add(txt_ontology_name);
-		*/
+		addButton.setText(ApplicationManager.getApplicationManager().getTraduction("addbutton"));
+		this.add(this.addButton, "5, 2, 5, 2");
+		this.addButton.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e)
+			{
+				RelationsEditorDialog.this.addInput();
+			}
+		});
 		
-		// Disposition de la fenetre
-		this.setVisible(true);
-		this.setSize(500,500);
-		this.setResizable(false);
-		this.pack();
+		delButton.setText(ApplicationManager.getApplicationManager().getTraduction("delbutton"));
+		this.add(this.delButton, "5, 3, 5, 3");
+		this.delButton.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e)
+			{
+				RelationsEditorDialog.this.delInput();
+			}
+		});
+		
+		this.setSize(450,350);
 		this.setLocation((frame.getLocation().x) + (frame.getSize().width/2) - (this.getSize().width/2),(frame.getLocation().y) + (frame.getSize().height/2) - (this.getSize().height/2));
+		this.setVisible(true);
 	}
 
-/**
+	/**
     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
  	*/
-	public void actionPerformed(ActionEvent e) {
-		this.value = VALIDATE_CAPTION;
-    }
-
-    /** 
-     * This method reacts to state changes in the option pane. 
-     * @param e The event to treat. 
-     */
-    public void propertyChange(PropertyChangeEvent e) {
-        String prop = e.getPropertyName();
-
-        if (isVisible() && (JOptionPane.VALUE_PROPERTY.equals(prop) || JOptionPane.INPUT_VALUE_PROPERTY.equals(prop))) {
-            Object temp_value = this.value;
-
-            if (temp_value == JOptionPane.UNINITIALIZED_VALUE) {
-                // Ignore reset
-                return;
-            }
-
-            // Reset the JOptionPane's value.
-            // If you don't do this, then if the user
-            // presses the same button next time, no
-            // property change event will be fired.
-            this.value = JOptionPane.UNINITIALIZED_VALUE;
-
-            if (VALIDATE_CAPTION.equals(temp_value)) {
-				String err_msg = "";
-				if (this.txt_ontology_name.getText().equals(""))
-					err_msg += "Le nom de l'ontologie ne doit pas \u00eatre vide. \n";
-				if (!com.sun.org.apache.xerces.internal.util.URI.isConformantSchemeName(this.txt_storage_repository.getText()))
-					err_msg += "L'emplacement doit \u00eatre une URI (chemin d'acc\u00e8s ou URL). \n";
-				if (!err_msg.equals(""))
-					err_msg = "Des erreurs de saisies ont \u00e9t\u00e9 rep\u00e9r\u00e9es : \n";
-				else {
-					// TODO Coder l'enregistrement de l'ontologie.
-					this.setVisible(false);
-				}
-            } else {
-				this.setVisible(false);
-            }
-        }
-    }
-
-	private void validateInput()
+	public void actionPerformed(ActionEvent e)
 	{
-		String err_msg = "";
-		if (this.txt_ontology_name.getText().equals(""))
-			err_msg += "\t- Nom de l'ontologie non sp\u00e9cifi\u00e9. \n";
-		if (this.cmb_storage_type.getSelectedIndex() == 0) {
-			if (this.txt_storage_repository.equals(""))
-				err_msg += "\t- Emplacement du d\u00e9p\u00f4t non sp\u00e9cifi\u00e9. \n";
-		}
-		if (this.txt_storage_repository.equals("")) {
-			if (this.cmb_storage_type.getSelectedIndex() == 0)
-				err_msg += "\t- Type de stockage des donn\u00e9es non sp\u00e9cifi\u00e9. \n";
-		}
-//		if (!com.sun.org.apache.xerces.internal.util.URI.isWellFormedAddress(this.txt_storage_repository.getText()))
-//			err_msg += "\t- L'emplacement doit être une URI (chemin d'accès ou URL). \n";
-		if (!String.valueOf(this.txt_user_confirm.getPassword()).equals(String.valueOf(this.txt_user_password.getPassword()))) {
-			err_msg += "\t- Le mot de passe est erron\u00e9. \n";
-			this.txt_user_password.setText("");
-			this.txt_user_confirm.setText("");
-		}
-		if (!err_msg.equals("")) {
-			err_msg = "Des erreurs de saisies ont \u00e9t\u00e9 rep\u00e9r\u00e9es : \n" + err_msg;
-			Object[] options = {"OK"};
-			JOptionPane.showOptionDialog(this, err_msg, "Erreur de saisie", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-		}
-		else {
-			try {
-				EncryptedProperties.createPropertiesFile(
-						this.txt_ontology_name.getText(), 
-						((StorageCell) this.cmb_storage_type.getSelectedItem()).getDriver(), 
-						this.txt_storage_repository.getText(), 
-						this.txt_user_login.getText(), 
-						String.valueOf(this.txt_user_password.getPassword()));
-				this.valid = true;
-				this.setVisible(false);
-			} catch (InvalidKeyException e) {
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors du cryptage du mot de passe : clef invalide. ", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			} catch (NoSuchAlgorithmException e) {
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors du cryptage du mot de passe : algorithme invalide. ", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			} catch (NoSuchPaddingException e) {
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors du cryptage du mot de passe : padding inexistant. ", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			} catch (IllegalBlockSizeException e) {
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors du cryptage du mot de passe : taille des blocs invalide. ", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			} catch (BadPaddingException e) {
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors du cryptage du mot de passe : padding invalide. ", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			} catch (IOException e) {
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors de la création du fichier ./" + Constants.DEFAULT_ONTOLOGIES_PATH  + this.txt_ontology_name.getText() + ".properties", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			} catch (InvalidKeySpecException e)
-			{
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors du cryptage du mot de passe : spécification de clef invalide. ", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			} catch (InvalidAlgorithmParameterException e)
-			{
-				Object[] options = {"OK"};
-				JOptionPane.showOptionDialog(this, "Une erreur interne est survenue lors du cryptage du mot de passe : algorithme invalide. ", "Erreur de sauvegarde", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-				e.printStackTrace();
-			}
-		}
+
+    }
+
+    private void validateInput()
+    {
+    	validInput();
+    	dispose();
+    }
+
+    private void cancelInput()
+    {
+    	this.dispose();
+    }
+    
+    private void delInput()
+    {
+    	int [] sr = liste.getSelectedRows();
+    	if(sr.length>0)
+    	{
+    		model.delRows(sr);
+    	}
+    	liste.changeSelection(0,0,false,false);
+    	liste.updateUI();
 	}
 
-   private void cancelInput()
-   {
-	   this.setVisible(false);
-   }
+    private void addInput()
+    {
+		String nom = textField.getText();
+    	if(!nom.equals(""))	model.addRow(nom);
+    	textField.setText("");
+    	liste.updateUI();
+	}
+    
+    private class StorageCell
+    {
+    	private String name = "";
+    	private String driver = "";
    
-   private class StorageCell {
-	   private String name = "";
-	   private String driver = "";
-	   
-	   /**
-	    * @param name The displayable name of this cell. 
-	    * @param driver The Java class name of the <code>IService</code> to use for this kind of storage.
-	    */
-	   public StorageCell(String name, String driver) {
-		   this.name = name;
-		   this.driver = driver;
-	   }
+		/**
+		* @param name The displayable name of this cell. 
+		* @param driver The Java class name of the <code>IService</code> to use for this kind of storage.
+		*/
+		public StorageCell(String name, String driver)
+		{
+			this.name = name;
+			this.driver = driver;
+		}
 	   
 	   /**
 	    * @return The Java class name of the <code>IService</code> to use for this kind of storage. 
@@ -342,13 +206,6 @@ public class RelationsEditorDialog extends JDialog  implements ActionListener, P
 	 */
 	public boolean validInput()
 	{
-		return this.valid;
-	}
-	
-	/**
-	 * @return The typed name of the new <code>Ontology</code> if the input has been validated. 
-	 */
-	public String getOntologyname() {
-		return this.validInput() ? this.txt_ontology_name.getText() : null;
+		return true;
 	}
 }
---
+++ Arkeo/ontologyEditor/gui/tables/RelationsEditorTableModel.java
@@ -0,0 +1,175 @@
+/**
+ * Created on 23 mai 2005
+ * 
+ * Arkeotek Project
+ */
+package ontologyEditor.gui.tables;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Vector;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.AbstractTableModel;
+
+import ontologyEditor.ApplicationManager;
+import ontologyEditor.DisplayManager;
+
+import arkeotek.ontology.DocumentPart;
+import arkeotek.ontology.Lemma;
+import arkeotek.ontology.LinkableElement;
+import arkeotek.ontology.Relation;
+
+/**
+ * @author Bernadou Pierre 
+ * @author Czerny Jean
+ *
+ */
+public class RelationsEditorTableModel extends EditorTableModel
+{
+	protected ArrayList<Object> relations = null;
+	protected String titre = null;
+	
+
+
+	/**
+	 * Initialize attributes ..
+	 * list of the kind of relations ..
+	 * Column name ..
+	 */
+	public RelationsEditorTableModel()
+	{
+		super(new Lemma("JeNeSertARien"));
+		
+		ArrayList<LinkableElement> aLR = null;
+		try
+		{
+			aLR = ApplicationManager.ontology.get(Relation.KEY);
+		}
+		catch(NullPointerException exception)
+		{
+			JOptionPane.showMessageDialog(DisplayManager.mainFrame,"Veuillez ouvrir une ontologie","Pas d'ontologie chargée",JOptionPane.INFORMATION_MESSAGE);
+		}
+		this.relations = new ArrayList<Object>();
+		for(int i=0;i<aLR.size();i++)
+		{
+			this.relations.add(aLR.get(i));
+		}
+		
+		this.titre = ""; // ApplicationManager.getApplicationManager().getTraduction("relations");
+	}
+	
+	
+	
+	
+	
+	/*
+	 * Méthodes à implémenter
+	 * **********************
+	 * 
+	 */
+	
+	// Retourne le nombre de colonnes
+	public int getColumnCount()
+	{
+		if (this.getElement() != null)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	// Retourne le nombre de lignes
+	public int getRowCount()
+	{
+		if (this.getElement() != null)
+			return relations.size();
+		return 0;
+	}
+	
+	// Retourne le nom de la colonne
+	public String getColumnName(int column)
+	{
+		if(column==0)
+		{
+			return(this.titre);
+		}
+		else
+		{
+			return("");
+		}
+	}
+	
+	// Retourne le type d'objet de la colonne passée en paramètre
+	public Class getColumnClass(int columnIndex)
+	{
+		return Relation.class;
+	}
+
+	// Retourne si les cellules sont éditables
+	public boolean isCellEditable(int rowIndex, int columnIndex)
+	{
+		return true;
+	}
+
+	// Retourne l'objet se trouvant à la ligne rowIndex et colonne columnIndex
+	public Object getValueAt(int rowIndex, int columnIndex)
+	{
+		return this.relations.get(rowIndex);
+	}
+
+	// Suppression des lignes désignées par un ensemble d'index
+	public void delRows(int[] sr)
+	{
+		for(int i=sr.length-1; i>=0;i--)
+		{
+			int rowIndex = sr[i];
+			this.relations.remove(rowIndex);
+		}
+	}
+
+	public void addRow(String nom)
+	{
+		try
+		{
+			boolean res = false;
+			for(int i=0;i<relations.size();i++)
+			{
+				if(nom.equals(((Relation) relations.get(i)).getName())) res=true;
+			}
+			if(!res)
+			{
+				relations.add(new Relation(nom));
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(DisplayManager.mainFrame,"Nom deja utilise");
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void removeRelation(int lineSelected) throws Exception
+	{
+		int [] tab = {lineSelected};
+		delRows(tab);
+	}
+
+	public void removeElement(int lineSelected) throws Exception
+	{
+		int [] tab = {lineSelected};
+		delRows(tab);
+	}
+	
+	/*
+	 * 
+	 * Fin des méthodes à implémenter
+	 * 
+	 */
+}
