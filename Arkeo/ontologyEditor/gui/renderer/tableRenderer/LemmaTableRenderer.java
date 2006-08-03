a0dc9cba12a156f7b3971787a1448f4a92c93708
aldonit <aldonit>
2006-08-03 08:16:15 +0000

    *** empty log message ***

--- Arkeo/ontologyEditor/gui/renderer/tableRenderer/LemmaTableRenderer.java
+++ Arkeo/ontologyEditor/gui/renderer/tableRenderer/LemmaTableRenderer.java
@@ -17,19 +17,27 @@ import ontologyEditor.ApplicationManager;
 import ontologyEditor.Constants;
 import ontologyEditor.ImagesManager;
 
+/*
+ * Julien Sanmartin
+ * Classe permettant de parametrer l'affichage dans la table des lemmes
+ */
+
 public class LemmaTableRenderer extends DefaultTableCellRenderer  {
 	
-	
+	// cette fonction est appelée à chaque raffraichissement de l'interface
 	public Component getTableCellRendererComponent (JTable table, Object value, boolean isSelected,
 	        boolean hasFocus, int row, int column) {
 	    
 	    Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	    // si value appartient à LemmeValider de la classe Ontology alors on change la couleur des caracteres 
+	    // de la ligne coorespondante a value
 	    if (ApplicationManager.ontology.getLemmeValider().contains(value))
 	    {
 	    	c.setForeground(new Color(0,102,0));
 	    }
 	    else
 	    {
+	    	// sinon on met la couleur de la ligne en noir
 	    	c.setForeground(Color.BLACK);
 	    }
 	    return c;
--- Arkeo/ontologyEditor/gui/renderer/tableRenderer/LemmeEditionRenderer.java
+++ Arkeo/ontologyEditor/gui/renderer/tableRenderer/LemmeEditionRenderer.java
@@ -15,23 +15,33 @@ import arkeotek.ontology.Lemma;
 
 import ontologyEditor.ApplicationManager;
 
+/*
+ * Julien Snamartin
+ * Classe permettant de parametrer l'affichage dans la table BottomEdition
+ */
+
 public class LemmeEditionRenderer extends DefaultTableCellRenderer  {
 	
-	
+	// cette fonction est appelée à chaque raffraichissement de l'interface
 	public Component getTableCellRendererComponent (JTable table, Object value, boolean isSelected,
 	        boolean hasFocus, int row, int column) {
 	    
 	    Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	    // si value est une instance de la classe Lemma alors on change la couleur des caracteres 
+	    // de la ligne coorespondante a value
 	    if (value instanceof Lemma)
 	    {
     		c.setForeground(new Color(0,102,0));
 	    }
+	    // si value est une instance de la classe DocumentPart alors on change la couleur des caracteres 
+	    // de la ligne coorespondante a value
 	    else if (value instanceof DocumentPart)
 	    {
 	    	c.setForeground(new Color(102,51,0));
 	    }
 	    else
 	    {
+	    	// sinon on met la couleur de la ligne en noir
 	    	c.setForeground(Color.BLACK);
 	    }
 	    
